ffcb10f2f3ee185b4654c371dba038ef8a93bcc7
Carolina <naccaratocarolina@dcc.ufrj.br>
2021-04-11 17:28:48 -0300

    + Quick Sort

--- ordenaVetor.c
+++ ordenaVetor.c
@@ -2,8 +2,33 @@
 #include<stdlib.h>
 #include <string.h>
 
-void  quickSort() {
-	printf("quick sort");
+void exchange(int* firtsElement, int* secondElement) {
+	int temporaryNumber = *firtsElement;
+	*firtsElement = *secondElement;
+	*secondElement = temporaryNumber;
+}
+
+int partition(int* arrayOfNumbers, int start, int end) {
+	int pivot = arrayOfNumbers[end];
+	int i = (start - 1);
+
+	for (int j=start; j<=end - 1; j++) {
+		if (arrayOfNumbers[j] <= pivot) {
+			i += 1;
+			exchange(&arrayOfNumbers[i], &arrayOfNumbers[j]);
+		}
+	}
+	
+	exchange(&arrayOfNumbers[i + 1], &arrayOfNumbers[end]);
+	return (i + 1);
+}
+
+void  quickSort(int* arrayOfNumbers, int start, int end) {
+	if (start < end) {
+		int part = partition(arrayOfNumbers, start, end);
+		quickSort(arrayOfNumbers, start, part - 1);
+		quickSort(arrayOfNumbers, part + 1, end);
+	}
 }
 
 void mergeSort() {
@@ -12,6 +37,12 @@ void mergeSort() {
 
 void quadraticAlgorithm() {
 	printf("quadratico");
+}
+
+void printArrayOfNumbers(int* arrayOfNumbers, int arraySize) {
+	for (int i=0; i<arraySize; i++) {
+		printf("%d\n", arrayOfNumbers[i]);
+	}
 }
 
 int* dynamicallyFillArray(int* arraySize, int* arrayOfNumbers) {
@@ -34,19 +65,18 @@ int main (int argc, char *argv[]) {
 	int arraySize;
 
 	arrayOfNumbers = dynamicallyFillArray(&arraySize, arrayOfNumbers);
+	printArrayOfNumbers(arrayOfNumbers, arraySize);
 
-	for (int i=0; i<arraySize; i++) {
-		printf("%d\n", arrayOfNumbers[i]);
-	}
-	
-	printf("%s", argv[1]);
 	// Seleciona o algoritmo desejado
 	if (argc == 1) quadraticAlgorithm();
 	else {
-		if (strcmp(argv[1], "-q") == 0) quickSort();
+		if (strcmp(argv[1], "-q") == 0) quickSort(arrayOfNumbers, 0, arraySize - 1);
 		else if (strcmp(argv[1], "-m") == 0) mergeSort();
 	}
-
+	
+	printf("%s", "\n");
+	printArrayOfNumbers(arrayOfNumbers, arraySize);
+	
 	free(arrayOfNumbers);
 
 	return(0);
